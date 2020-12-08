291d6ac5de7712a528459d7cb00832826ea9b2fb
Gerson <gerson@cloud-america.com>
2020-12-08 16:52:16 -0400

    Adding scripts for actions

---
+++ src/study_case_1_plot.py
@@ -0,0 +1,53 @@
+from pytrends.request import TrendReq
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import os
+
+plt.style.use("seaborn-darkgrid")
+FONT_SIZE = 22
+plt.rcParams.update({'font.size': FONT_SIZE})
+LOCATION_OF_STUDY = "CO"
+DATE_FORMAT = "%Y-%m-%d"
+MAIN_KEYWORD = "covid-19"
+SECONDARY_KEYWORD = os.environ.get("SECONDARY_KEYWORD","cursos online")
+
+def get_interest_overtime_given_a_keyword_and_location(keyword: str, 
+                        location: str = LOCATION_OF_STUDY) -> pd.DataFrame:
+    try:
+        trend_request = TrendReq()
+        trend_request.build_payload(kw_list=[keyword],geo=location)
+        return trend_request.interest_over_time()
+    except Exception as error:
+        print(f"Error {error} has happened, please check the arguments")
+
+def get_slice_of_df_given_years(df: pd.DataFrame, start: str, end: str) -> pd.DataFrame:
+    return df.loc[(df.index > start) & (df.index < end)]
+
+
+covid_19_interest = get_interest_overtime_given_a_keyword_and_location(MAIN_KEYWORD)
+secondary_interest = get_interest_overtime_given_a_keyword_and_location(SECONDARY_KEYWORD)
+
+covid_19_interest_2020 = get_slice_of_df_given_years(covid_19_interest,"2020", "2021")
+secondary_interest_2020 = get_slice_of_df_given_years(secondary_interest,"2020", "2021")
+
+covid_19_interest_2020 = covid_19_interest_2020[covid_19_interest_2020.index != "2020-08-02"]
+secondary_interest_2020 = secondary_interest_2020[secondary_interest_2020.index != "2020-08-02"]
+
+fig, (ax1, ax2) = plt.subplots(1,2)
+fig.set_figheight(16)
+fig.set_figwidth(21)
+ax1.plot(covid_19_interest_2020.index, covid_19_interest_2020[MAIN_KEYWORD])
+ax1.set_xlabel("date", fontsize = FONT_SIZE)
+ax1.set_ylabel("keyword: '" + MAIN_KEYWORD + "' interest", fontsize = FONT_SIZE)
+fig.autofmt_xdate()
+sns.regplot(x = covid_19_interest_2020[MAIN_KEYWORD], 
+            y = secondary_interest_2020[SECONDARY_KEYWORD], 
+            ax = ax2)
+ax2.set_xlabel("keyword: '" + MAIN_KEYWORD + "' interest", fontsize = FONT_SIZE)
+ax2.set_ylabel("keyword: '" + SECONDARY_KEYWORD + "' interest", fontsize = FONT_SIZE)
+plt.tight_layout()
+plt.savefig("impact.png",dpi=120)
+plt.close()
+
+
---
+++ src/study_case_2_plot.py
@@ -0,0 +1,93 @@
+from model import CaseLogisticModel
+from google.oauth2 import service_account
+from google.cloud import bigquery
+from matplotlib import pyplot as plt
+import os
+
+plt.style.use("seaborn-darkgrid")
+FONT_SIZE = 22
+plt.rcParams.update({'font.size': FONT_SIZE})
+# Caso de estudio y pais de estudio 
+COUNTRY_OF_STUDY = os.environ.get("COUNTRY_OF_STUDY","Colombia")
+CASE_OF_STUDY = os.environ.get("CASE_OF_STUDY","cumulative_confirmed")
+
+
+# Credencials para bigquery
+credentials_dict = {
+    'auth_provider_x509_cert_url': os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
+    'auth_uri': os.getenv("AUTH_URI"),
+    'client_email': os.getenv("CLIENT_EMAIL"),
+    'client_id':  os.getenv('CLIENT_ID'),
+    'client_x509_cert_url': os.getenv('CLIENT_X509_CERT_URL'),
+    'private_key': os.getenv('PRIVATE_KEY').replace("\\n","\n"),
+    'private_key_id': os.getenv('PRIVATE_KEY_ID'),
+    'project_id': os.getenv('PROJECT_ID'),
+    'token_uri': os.getenv('TOKEN_URI'),
+    'type': os.getenv('TYPE')
+}
+
+credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+
+# Cliene bigquery
+client = client = bigquery.Client(credentials=credentials, 
+                        project=credentials_dict["project_id"])
+
+# query string para hacer la prediccion en funcion de los datos
+query = f'''
+SELECT date, {CASE_OF_STUDY}
+FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
+WHERE country_name = "{COUNTRY_OF_STUDY}" AND aggregation_level = 0
+'''
+
+try:
+    samples = client.query(query).result().to_dataframe()
+except Exception as error:
+    raise ValueError(f"Error {error} has happened please check the arguments")
+
+# Eliminando datos Nan 
+samples = samples[samples["cumulative_confirmed"] != 0].dropna()
+
+# Instanciando modelo
+model = CaseLogisticModel(COUNTRY_OF_STUDY, CASE_OF_STUDY)
+
+# Ajustando modelo
+model.fit(samples, printing = False)
+
+# Variables de interes
+predicted_last_day_of_case = model.predict_last_day()
+amount_of_case_at_last_day = model.predict_last_day_amount_case()
+
+with open("text.txt","w") as outfile:
+    outfile.write(f"Model predicted last day: {predicted_last_day_of_case} \n")
+    outfile.write(f"Model predicted amount of afected at last day: {amount_of_case_at_last_day} \n")
+    outfile.write("\n")
+    outfile.write("Model params and erros:")
+    a = int(model.logistic_params_case["a"])
+    a_error = round(model.logistic_params_error_case["a_error"],4)
+    b = int(model.logistic_params_case["b"])
+    b_error = round(model.logistic_params_error_case["b_error"],4)
+    c = int(model.logistic_params_case["c"])
+    c_error = round(model.logistic_params_error_case["c_error"],4)
+    outfile.write(f"a = {a} +/- {a_error} \n")
+    outfile.write(f"b = {b} +/- {b_error} \n")
+    outfile.write(f"c = {c} +/- {c_error} \n")
+    outfile.write("\n")
+
+model_prediction_day = "2021-01-01"
+model_samples = model.get_behavior_until_a_date(model_prediction_day)
+fig, ax = plt.subplots()
+fig.set_figheight(13)
+fig.set_figwidth(17)
+ax.plot(samples["date"], 
+        samples["cumulative_confirmed"],
+        c = "salmon", label =  f"{CASE_OF_STUDY} cases", marker = '.')
+ax.plot(model_samples["date"],
+       model_samples["cumulative_confirmed"], 
+        c = "orangered",label = f"{CASE_OF_STUDY} prediction")
+fig.autofmt_xdate()
+ax.set_xlabel("Date")
+ax.set_ylabel(f"{CASE_OF_STUDY} cases")
+fig.legend()
+plt.tight_layout()
+plt.savefig("prediction.png",dpi=120)
+plt.close()
